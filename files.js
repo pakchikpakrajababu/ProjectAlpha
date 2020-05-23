955346200ef963554ad000c00fd50a6ef2a9d6f6
vaibhav.a.tiwari <vaibhav.a.tiwari@accenture.com>
2020-05-23 18:01:48 +0530

    adding analytics

--- files.js
+++ files.js
@@ -149,6 +149,8 @@ jQuery(function($) {
   }); */
   /*******SCROLL TO TOP -- START ********/
   $(document).ready(function() {
+  $(window).on("scroll", doAnimations);
+  $(window).trigger("scroll");
 	$(this).scrollTop(0);
   $(".title_logo").hide();
   if ($(window).height() < 425) {
@@ -233,3 +235,28 @@ $(window).resize(function() {
     $(".title_logo").hide();
   }
 });
+
+  // Function which adds the 'animated' class to any '.animatable' in view
+  var doAnimations = function() {
+    
+    // Calc current offset and get all animatables
+    var offset = $(window).scrollTop() + $(window).height(),
+        $animatables = $('.animatable');
+    
+    // Unbind scroll handler if we have no animatables
+    if ($animatables.length == 0) {
+      $(window).off('scroll', doAnimations);
+    }
+    
+    // Check all animatables and animate them if necessary
+		$animatables.each(function(i) {
+       var $animatable = $(this);
+			if (($animatable.offset().top + $animatable.height() - 20) < offset) {
+        $animatable.removeClass('animatable').addClass('animated');
+			}
+    });
+
+	};
+  
+  // Hook doAnimations on scroll, and trigger a scroll
+
