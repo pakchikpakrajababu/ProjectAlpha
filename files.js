14692130da8e1b3bc9999c50b2621e028e8e8ff7
vaibhav.a.tiwari <vaibhav.a.tiwari@accenture.com>
2020-06-01 19:56:54 +0530

    fixing production build

--- files.js
+++ files.js
@@ -99,6 +99,17 @@ window.onload = function () {
           siteCarousel();
         }
         /****************Slider -- End***************/
+         var isChrome =
+           /Chrome/.test(navigator.userAgent) &&
+           /Google Inc/.test(navigator.vendor);
+         if (window.location.hash && isChrome) {
+           setTimeout(function () {
+             var hash = window.location.hash;
+             window.location.hash = "";
+             window.location.hash = hash;
+           }, 300);
+         }
+        
 
         $(window).on("scroll", doAnimations);
         $(window).trigger("scroll");
